f1f08f4ea2ffe291e2f27f52d642a143e2e6c16e
Jason Rotella <jason.rotella@ge.com>
2017-04-01 17:35:47 -0400

    Export of I2cDevice and updates to RPi Adaptor for consistency with GPIO and I2C changes

--- gobot/platforms/raspi/doc.go
+++
@@ -1,5 +0,0 @@
-/*
-Package raspi contains the emulated Gobot adaptor for the Raspberry Pi.
-*/
-
-package raspi
--- gobot/platforms/raspi/raspi_adaptor.go
+++ gobot/platforms/raspi/raspi_adaptor.go
@@ -3,9 +3,8 @@ package raspi
 import (
 	"errors"
 	"fmt"
-	"os"
-	//"github.com/hybridgroup/gobot"
-	//"github.com/hybridgroup/gobot/sysfs"
+
+	"github.com/Jay-AHR/raspi_em/gobot/sysfs"
 )
 
 // Removed readFile() as it was used to set i2c revision
@@ -107,8 +106,6 @@ var pins = map[string]map[string]int{
 	},
 }
 
-// NewRaspiAdaptor creates a RaspiAdaptor with specified name and
-// DigitalPin mapping (no PWM pins for RPi)
 func NewRaspiAdaptor(name string) *RaspiAdaptor {
 	r := &RaspiAdaptor{
 		name:        name,
@@ -142,7 +139,7 @@ func (r *RaspiAdaptor) translatePin(pin string) (i int, err error) {
 	// JER: check if per board-revision pins are valid
 	if val, ok := pins[pin][r.revision]; ok {
 		i = val
-		// JER: check if pins present on all board revisions are valid
+		// JER: check if pins that are present on all board revisions are valid
 	} else if val, ok := pins[pin]["*"]; ok {
 		i = val
 	} else {
@@ -176,6 +173,7 @@ func (r *RaspiAdaptor) pwmPin(pin string) (i int, err error) {
 
 // digitalPin returns matched digitalPin for specified values
 func (r *RaspiAdaptor) digitalPin(pin string, dir string) (sysfsPin sysfs.DigitalPin, err error) {
+	//  JER: check if a valid pin via the "pin" mapping
 	i, err := r.translatePin(pin)
 
 	if err != nil {
@@ -214,60 +212,14 @@ func (r *RaspiAdaptor) DigitalWrite(pin string, val byte) (err error) {
 	return sysfsPin.Write(int(val))
 }
 
-// I2cStart starts a i2c device in specified address
-func (r *RaspiAdaptor) I2cStart(address int) (err error) {
-	if r.i2cDevice == nil {
-		r.i2cDevice, err = sysfs.NewI2cDevice(r.i2cLocation, address)
-	}
-	return err
-}
-
-// I2CWrite writes data to i2c device
-func (r *RaspiAdaptor) I2cWrite(address int, data []byte) (err error) {
-	if err = r.i2cDevice.SetAddress(address); err != nil {
-		return
-	}
-	_, err = r.i2cDevice.Write(data)
-	return
-}
+// JER: Removed I2cStart, I2cWrite as QPID does not use these
+// TODO: Add I2C methods back in, once QPID prototype is operational
 
 // I2cRead returns value from i2c device using specified size
-func (r *RaspiAdaptor) I2cRead(address int, size int) (data []byte, err error) {
-	if err = r.i2cDevice.SetAddress(address); err != nil {
-		return
-	}
-	data = make([]byte, size)
-	_, err = r.i2cDevice.Read(data)
-	return
-}
+func (r *RaspiAdaptor) I2cRead(address int, size int) (t int, err error) {
+	fmt.Println(address)
+	fmt.Println(size)
 
-func (r *RaspiAdaptor) PwmWrite(pin string, val byte) (err error) {
-	sysfsPin, err := r.pwmPin(pin)
-	if err != nil {
-		return err
-	}
-	return r.piBlaster(fmt.Sprintf("%v=%v\n", sysfsPin, gobot.FromScale(float64(val), 0, 255)))
-}
-
-func (r *RaspiAdaptor) ServoWrite(pin string, angle byte) (err error) {
-	sysfsPin, err := r.pwmPin(pin)
-	if err != nil {
-		return err
-	}
-
-	val := (gobot.ToScale(gobot.FromScale(float64(angle), 0, 180), 0, 200) / 1000.0) + 0.05
-
-	return r.piBlaster(fmt.Sprintf("%v=%v\n", sysfsPin, val))
-}
-
-func (r *RaspiAdaptor) piBlaster(data string) (err error) {
-	fi, err := sysfs.OpenFile("/dev/pi-blaster", os.O_WRONLY|os.O_APPEND, 0644)
-	defer fi.Close()
-
-	if err != nil {
-		return err
-	}
-
-	_, err = fi.WriteString(data)
-	return
+	t, err = r.i2cDevice.Read()
+	return t, err
 }
